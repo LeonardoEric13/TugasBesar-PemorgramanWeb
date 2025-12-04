d679b929e34fc4ca5a2529296587b428aa91b1c2
Lucid <leoericc13@gmail.com>
2025-12-04 19:51:45 +0700

    Update dashboard.php

--- TUBES(90%)/dashboard.php
+++ TUBES(90%)/dashboard.php
@@ -1,95 +1,57 @@
 <?php
 session_start();
-require_once '../config/koneksi.php';
 
+include 'config/koneksi.php';
 
-
-$error_message = '';
-$success_message = '';
-$page_title = 'Registrasi Member';
-
-
-
-if (isset($_POST['daftar'])) {
-    $nama = mysqli_real_escape_string($conn, $_POST['nama']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    
-    // Cek nama dan email sudah terdaftar
-    $cek_email = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
-    $cek_nama = mysqli_query($conn, "SELECT * FROM users WHERE nama_lengkap = '$nama'");
-    
-    $email_ada = mysqli_num_rows($cek_email) > 0;
-    $nama_ada = mysqli_num_rows($cek_nama) > 0;
-    
-    if ($email_ada && $nama_ada) {
-        $error_message = 'Email dan Username sudah digunakan!';
-    } else if ($email_ada) {
-        $error_message = 'Email sudah terdaftar!';
-    } else if ($nama_ada) {
-        $error_message = 'Nama sudah terdaftar!';
-    } else {
-        $sql = "INSERT INTO users (nama_lengkap, email, password) VALUES ('$nama', '$email', '$password')";
-        if (mysqli_query($conn, $sql)) {
-            $success_message = 'Registrasi berhasil! Silakan login.';
-        } else {
-            $error_message = 'Gagal mendaftar. Coba lagi.';
-        }
-    }
+// Cek sudah login atau belum
+if (!isset($_SESSION['login'])) {
+    header("Location: auth/login.php");
+    exit();
 }
+
+$nama_user  = $_SESSION['nama'];
+$page_title = "Dashboard";
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php echo $page_title; ?></title>
-    <link rel="stylesheet" type="text/css" href="../assets/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/style.css" />
 </head>
-<body>
+<body class="has-sidebar">
+
 <div id="wrapper">
     <div id="header">
-        <h1>Registrasi Member</h1>
+        <h1>Dashboard Member</h1>
     </div>
-    
-    <div id="content" style="float:none; width:auto; text-align:center;">
-        <?php if (!empty($success_message)): ?>
-            <div class="success-box">
-                <p><?php echo $success_message; ?></p>
-            </div>
-        <?php endif; ?>
-        
-        <?php if (!empty($error_message)): ?>
-            <div class="error-box">
-                <p><?php echo $error_message; ?></p>
-            </div>
-        <?php endif; ?>
-        
-        <form action="" method="post">
-            <div>
-                <label>Nama Lengkap:</label>
-                <input type="text" name="nama" required="required" />
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type="email" name="email" required="required" />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" name="password" minlength="6" required="required" />
-            </div>
-            <div>
-                <input type="submit" name="daftar" value="Daftar" style="width:100px;" />
-            </div>
-        </form>
-        
-        <p><a href="login.php">Sudah punya akun? Login disini</a></p>
-        <p><a href="../index.html">Kembali ke Halaman Depan</a></p>
+
+    <div id="nav">
+        <ul>
+            <li><a href="dashboard.php">Home</a></li>
+            <li><a href="pages/booking.php">Sewa Lapangan</a></li>
+            <li><a href="pages/riwayat.php">Riwayat</a></li>
+            <li><a href="pages/profil.php">Profil</a></li>
+        </ul>
+
+        <div id="logout-section">
+            <a href="auth/logout.php"
+               onclick="return confirm('Apakah Anda yakin ingin keluar?');">Logout</a>
+        </div>
     </div>
-    
+
+    <div id="content">
+        <h2>Halo, <?php echo htmlspecialchars($nama_user); ?></h2>
+        <div class="info-box">
+            <p>Selamat datang di sistem. Silakan pilih menu di samping untuk melakukan transaksi.</p>
+        </div>
+    </div>
+
     <div id="footer">
         <p>&copy; 2025 Padel Center</p>
     </div>
 </div>
+
 </body>
 </html>
-
